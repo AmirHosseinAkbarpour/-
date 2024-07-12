f53b8c1484c9238d201bc5def0caae4876c9dd0a
AmirHosseinAkbarpour <amirhosseinakbarpour542@gmail.com>
2024-07-12 22:09:39 +0330

    Q11

--- New Text Document (11).cpp
+++ New Text Document (11).cpp
@@ -1,46 +1,28 @@
 #include <iostream>
 using namespace std;
 
-class Time {
-private:
-    int hours, minutes, seconds;
-
-public:
-    Time(int h = 0, int m = 0, int s = 0) {
-        if (h >= 0 && h < 24 && m >= 0 && m < 60 && s >= 0 && s < 60) {
-            hours = h;
-            minutes = m;
-            seconds = s;
-        } else {
-            cout << "Invalid time values!" << endl;
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
         }
     }
-
-    void printTime() {
-        cout << hours << ":" << minutes << ":" << seconds << endl;
-    }
-
-    void calculateDifference(Time t) {
-        int totalSec1 = hours  3600 + minutes  60 + seconds;
-        int totalSec2 = t.hours  3600 + t.minutes  60 + t.seconds;
-        int diffSeconds = abs(totalSec1 - totalSec2);
-
-        int h = diffSeconds / 3600;
-        int m = (diffSeconds % 3600) / 60;
-        int s = diffSeconds % 60;
-
-        cout << "Time difference: " << h << ":" << m << ":" << s << endl;
-    }
-};
+    return true;
+}
 
 int main() {
-    Time t1(10, 30, 45);
-    Time t2(8, 15, 20);
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
 
-    t1.printTime();
-    t2.printTime();
-
-    t1.calculateDifference(t2);
+    if (isPrime(number)) {
+        cout << number << " is a prime number." << endl;
+    } else {
+        cout << number << " is not a prime number." << endl;
+    }
 
     return 0;
 }
