8474d424fa931c3e8ffdd78136831a6764c93c29
AmirHosseinAkbarpour <amirhosseinakbarpour542@gmail.com>
2024-07-12 22:32:06 +0330

    Q31

---
+++ New Text Document (31).cpp
@@ -0,0 +1,63 @@
+include <iostream>
+
+#include <conio.h>
+
+using namespace std;
+
+int main()
+
+{
+
+char ch[16];
+
+for (int i = 0; i < 16; i++)
+
+{
+
+ch[i]=_getch();
+
+cout << "\nwrong char please enter numberic char at" << i<<"\r\n";
+
+for (int ii = 0; iii; ii++)
+
+{
+
+if ('0' > (ch[i]) || '9' < (ch[i])) {
+
+}
+
+cout <<< ch[ii];
+
+1--;
+
+}
+
+else
+
+{
+
+cout <<< ch[i];
+
+if (i == 3) cout << "-";
+
+if (i == 7)cout << "-"; if (i == 11) cout << "-";
+
+}
+
+}
+
+cout <<"\r\n";
+
+for (int i=0;116; 1++)
+
+{
+
+cout << ch[i];
+
+if (i == 3) cout << "-";
+
+if (i == 7) cout << "-"; if (i == 11) cout << "-";
+
+}
+
+}
