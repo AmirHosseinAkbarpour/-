f83213fb4be9fd7dbb26bfc94c32c65cb9103246
AmirHosseinAkbarpour <amirhosseinakbarpour542@gmail.com>
2024-07-12 22:06:14 +0330

    Q8

--- New Text Document (8).cpp
+++ New Text Document (8).cpp
@@ -1,48 +1,23 @@
 #include <iostream>
-#include <string>
-#include <cctype>
-
 using namespace std;
 
-int main() {
-    string password;
-    cout << "Enter your password: ";
-    cin >> password;
-
-    int length = password.length();
-    int complexity = 0;
-
-    if (length >= 6) complexity += 20;
+void reverseNumber(int num) {
+    if(num < 10) {
+        cout << num;
+        return;
+    }
     
-    for (char c : password) {
-        if (isdigit(c)) {
-            complexity += 20;
-            break;
-        }
-    }
+    cout << num % 10;
+    reverseNumber(num / 10);
+}
 
-    for (char c : password) {
-        if (!isalnum(c)) {
-            complexity += 20;
-            break;
-        }
-    }
-
-    for (char c : password) {
-        if (isupper(c)) {
-            complexity += 20;
-            break;
-        }
-    }
-
-    for (char c : password) {
-        if (islower(c)) {
-            complexity += 20;
-            break;
-        }
-    }
-
-    cout << "Password strength: " << complexity << "%" << endl;
-
+int main() {
+    int number;
+    cout << "Enter an integer: ";
+    cin >> number;
+    
+    cout << "Reversed number: ";
+    reverseNumber(number);
+    
     return 0;
 }
