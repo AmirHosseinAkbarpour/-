3c15f663507027e090351f83a759f1a650a6bda4
AmirHosseinAkbarpour <amirhosseinakbarpour542@gmail.com>
2024-07-12 22:10:02 +0330

    Q12

--- New Text Document (12).cpp
+++ New Text Document (12).cpp
@@ -2,36 +2,18 @@
 using namespace std;
 
 int main() {
-    int menuChoice;
-    double totalPrice, vat;
+    int num;
+    char ch;
 
-    cout << "لطفا منوی غذا را انتخاب کنید (1-5): ";
-    cin >> menuChoice;
+    cout << "Enter a number: ";
+    cin >> num;
 
-    switch(menuChoice) {
-        case 1:
-            totalPrice = 10.0;
-            break;
-        case 2:
-            totalPrice = 15.0;
-            break;
-        case 3:
-            totalPrice = 20.0;
-            break;
-        case 4:
-            totalPrice = 25.0;
-            break;
-        case 5:
-            totalPrice = 30.0;
-            break;
-        default:
-            cout << "انتخاب نامعتبر!" << endl;
-            return -1;
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    for (int i = 0; i < num; i++) {
+        cout << ch;
     }
-
-    vat = totalPrice * 0.09;
-    cout << "قیمت کل: " << totalPrice << endl;
-    cout << "مالیات بر ارزش افزوده: " << vat << endl;
 
     return 0;
 }
